846671bbb517b9072aadc388740bff51e5332f9b
Kinuyo Fujikawa <kinuyofujikawa@gmail.com>
2017-03-10 12:48:20 -0800

    Add submit event

--- js/components/list.js
+++ js/components/list.js
@@ -1,41 +1,44 @@
 import React from 'react';
 import Card from './card';
 
-function Button(props) {
-	return <button onClick={props.onClick}>{props.text}</button>;
-}
-
-function ActionLink(){
-	function handleClick(e) {
-		e.preventDefault();
-		console.log('The button was clicked.');
-	}
-}
-
 class List extends React.Component {
 	constructor(props){
 		super(props);
 		this.state = {
-			clicked: false
+			value: ''
 		};
-		this.onButtonClick = this.onButtonClick.bind(this);
+
+		this.handleChange = this.handleChange.bind(this);
+		this.handleSubmit = this.handleSubmit.bind(this);
 	}
 
-	onButtonClick(){
-		this.setState({
-			clicked: true
-		});
+	handleChange(event){
+		this.setState({value: event.target.value});
+	}
+
+	handleSubmit(event){
+		console.log("Submit event happened");
+		event.preventDefault();
+	}
+
+	renderCard(event){
+		return(
+			<Card text={this.state.value} />
+		)
 	}
 
 	render () {
 		return (
 			<div className="list">
 				<h1>{this.props.listTitle}</h1>
-				<Card text='I am a Card'/>
-				<Card text='I am an awesome Card'/>
-				<form>
-					<label>New Card: </label>
-					<input type="text" id="card" name="card-name"></input>
+				<Card text='I am a card 1'/>
+				<Card text='I am a card 2'/>
+				<Card submit={this.handleSubmit} change={this.onAddInputChanged}/>
+				<form onSubmit={this.handleSubmit}>
+					<label>
+						New Card: 
+						<input type="text" value={this.state.value} onChange={this.handleChange}/>
+					</label>
 					<button type="submit">Submit</button>
 				</form>
 			</div>
@@ -44,8 +47,3 @@ class List extends React.Component {
 }
 
 export default List;
-
-// When the input's onChange event is fired, it should call an onAddInputChanged callback passed via the List's props.
-// When the form is submitted, it should run event.preventDefault(), then call an onAddSubmit callback passed via the List's props.
-// Pass an onAddInputChanged and an onAddSubmit callback to your List components.
-// In the callbacks, print a logging message to the console to make sure your callbacks are working. You'll implement the actual card adding functionality in the next lesson.
